1027e316adcdd7c3ef6e79d64c1977873e626901
Svjat <svjatderev@gmail.com>
2025-06-25 12:30:55 -0400

    Reconfigured the file tree and added a tileset

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const DASH_SPEED = 4000.0
+const DASH_SPEED = 600.0
 var DASHING = false
 	
 func _physics_process(delta: float) -> void:
@@ -25,11 +25,11 @@ func _on_direction_dash_timer_timeout():
 func get_input(delta : float) -> void:
 	# Ground movement
 	if (is_on_floor()):
-		if Input.is_action_pressed("keyboard_right"): 
+		if Input.is_action_pressed("key_right"): 
 			velocity.x += SPEED * delta
-		if Input.is_action_pressed("keyboard_left"): 
+		if Input.is_action_pressed("key_left"): 
 			velocity.x -= SPEED * delta
-		if Input.is_action_just_pressed("keyboard_up"): 
+		if Input.is_action_just_pressed("key_up"): 
 			velocity.y -= DASH_SPEED * delta
 			$DirectionDashTimer.start()
 			DASHING = true
@@ -37,13 +37,13 @@ func get_input(delta : float) -> void:
 	# Small dash
 	elif (!DASHING and !is_on_floor()):
 		var small_dash_dir = Vector2.ZERO
-		if Input.is_action_just_pressed("keyboard_up"): small_dash_dir.y = -1
-		if Input.is_action_just_pressed("keyboard_down"): small_dash_dir.y = 1
-		if Input.is_action_just_pressed("keyboard_right"): small_dash_dir.x = 1
-		if Input.is_action_just_pressed("keyboard_left"): small_dash_dir.x = -1
+		if Input.is_action_just_pressed("key_up"): small_dash_dir.y = -1
+		if Input.is_action_just_pressed("key_down"): small_dash_dir.y = 1
+		if Input.is_action_just_pressed("key_right"): small_dash_dir.x = 1
+		if Input.is_action_just_pressed("key_left"): small_dash_dir.x = -1
 		
 		if (small_dash_dir != Vector2.ZERO):
-			velocity = small_dash_dir * DASH_SPEED/4
+			velocity = small_dash_dir * DASH_SPEED/2
 			$DirectionDashTimer.start()
 			DASHING = true
 			
@@ -51,6 +51,10 @@ func get_input(delta : float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
 	var player_pos = get_global_transform_with_canvas().get_origin() + Vector2($Sprite2D.texture.get_width()/2, $Sprite2D.texture.get_height()/2)
 	if Input.is_action_just_pressed("mouse_left"): 
-		velocity = (mouse_pos - player_pos).normalized() * DASH_SPEED/2
+		velocity = (mouse_pos - player_pos).normalized() * DASH_SPEED
+		
+	# Cancel all movement with space
+	if Input.is_action_just_pressed("key_space"):
+		velocity = Vector2.ZERO
 	
 	
