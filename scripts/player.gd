c46885a07b39863c657c19bc76c3eca8e12de5cc
Svjat <svjatderev@gmail.com>
2025-06-28 12:27:15 -0400

    Rewrote the player movement script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,63 +1,80 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
-const DASH_SPEED = 600.0
-var DIR_DASHING = false
-var DIR_DASH_CD = false
-	
+const accel = 800
+const decel = 400
+const max_speed = 2000
+# kdash is the keyboard dash; vdash is the mouse dash
+const kdash_speed_mult = 2.5
+const mdash_speed_mult = 5
+
+var key_press_input = Vector2.ZERO
+var key_held_input = Vector2.ZERO
+var mouse_press_input = Vector2.ZERO
+# only one type of dash can be active at a time
+var kdashing = false
+var mdashing = false
+var kdash_cd = false
+
 func _physics_process(delta: float) -> void:
-	get_input(delta)
-
-	# Add the gravity.
-	if (!is_on_floor() and !DIR_DASHING):
-		velocity.y += get_gravity().y/2 * delta
-
-	if (!DIR_DASHING):
-		velocity.x = move_toward(velocity.x, 0, 20) # Also would affect the speed of acceleration
-
+	player_movement(delta)
+	apply_physics(delta)
 	move_and_slide()
 
-func get_input(delta : float) -> void:
-	# Ground movement
-	if (is_on_floor()):
-		if Input.is_action_pressed("key_right"): 
-			velocity.x = SPEED
-		if Input.is_action_pressed("key_left"): 
-			velocity.x = -SPEED
-		if Input.is_action_just_pressed("key_up"): 
-			velocity.y -= DASH_SPEED/3
-			$DirectionDashTimer.start()
-			DIR_DASHING = true
+func player_movement(delta: float) -> void:
+	get_key_press_input()
+	get_key_held_input()
+	get_mouse_press_input()
 
-	# Small dash
-	elif (!DIR_DASHING and !DIR_DASH_CD and !is_on_floor()):
-		var small_dash_dir = Vector2.ZERO
-		if Input.is_action_just_pressed("key_up"): small_dash_dir.y = -1
-		if Input.is_action_just_pressed("key_down"): small_dash_dir.y = 1
-		if Input.is_action_just_pressed("key_right"): small_dash_dir.x = 1
-		if Input.is_action_just_pressed("key_left"): small_dash_dir.x = -1
+	if (mouse_press_input != Vector2.ZERO and !mdashing and !kdashing):
+		mdashing = true
+		$MDashTimer.start()
+		handle_dashing(delta)
 
-		if (small_dash_dir != Vector2.ZERO):
-			velocity = small_dash_dir * DASH_SPEED/2
-			$DirectionDashTimer.start()
-			DIR_DASHING = true
+	elif (key_press_input != Vector2.ZERO and !kdashing and !mdashing and !kdash_cd):
+		kdashing = true
+		$KDashTimer.start()
+		handle_dashing(delta)
 
-	# Mouse Dash
-	var mouse_pos = get_viewport().get_mouse_position()
-	var player_pos = get_global_transform_with_canvas().get_origin() + Vector2($Sprite2D.texture.get_width()/2, $Sprite2D.texture.get_height()/2)
+func apply_physics(delta: float) -> void:
+	if (key_press_input == Vector2.ZERO and !kdashing and !mdashing):
+		# set gravity to 500px/s from 980px/s
+		velocity += get_gravity() * delta
+		velocity.x = move_toward(velocity.x, 0, decel * delta)
+
+func get_key_press_input() -> void:
+	key_press_input.x = int(Input.is_action_just_pressed("key_right")) - int(Input.is_action_just_pressed("key_left"))
+	key_press_input.y = int(Input.is_action_just_pressed("key_down")) - int(Input.is_action_just_pressed("key_up"))
+	key_press_input.normalized()
+
+func get_key_held_input() -> void:
+	key_held_input.x = int(Input.is_action_pressed("key_right")) - int(Input.is_action_pressed("key_left"))
+	key_held_input.y = int(Input.is_action_pressed("key_down")) - int(Input.is_action_pressed("key_up"))
+	key_held_input.normalized()
+
+func get_mouse_press_input() -> void:
 	if Input.is_action_just_pressed("mouse_left"):
-		var difference = mouse_pos - player_pos
-		# Roughly a length of 300 from player to top/ bottom of screen, and a length of roughly 500 from either side of the screen
-		velocity = (difference).normalized() * (DASH_SPEED * clamp(difference.length(), 50, 200)/200) # Now speeda is determine be mouse distance from player
+		var mouse_pos = get_viewport().get_mouse_position()
+		var player_pos = get_global_transform_with_canvas().get_origin() + Vector2($Sprite2D.texture.get_width()/2, $Sprite2D.texture.get_height()/2)
+		mouse_press_input = mouse_pos - player_pos
 
-	# Cancel all movement with space
-	if Input.is_action_just_pressed("key_space"):
-		velocity = Vector2.ZERO	
+func handle_dashing(delta: float) -> void:
+	if (kdashing):
+		# the kdash timer handles the duration 
+		velocity = (key_held_input * max_speed * kdash_speed_mult * delta)
+	elif (mdashing):
+		# the mdash only has a duration handled by the mdash timer
+		# roughly a length of 300 from player to top/ bottom of screen, and a length of roughly 500 from either side of the screen
+		# speed is determined by the mouse distance from player
+		velocity = mouse_press_input.normalized() * (max_speed * mdash_speed_mult * (clamp(mouse_press_input.length(), 50, 200)/200) * delta) 
 
-func _on_direction_dash_timer_timeout():
-	DIR_DASHING = false
-	$DirectionDashCooldownTimer.start()
-	DIR_DASH_CD = true
+func _on_k_dash_timer_timeout() -> void:
+	kdashing = false
+	kdash_cd = true
+	$KDashCdTimer.start()
 
-func _on_direction_dash_cooldown_timer_timeout() -> void:
-	DIR_DASH_CD = false
+func _on_m_dash_timer_timeout() -> void:
+	mdashing = false
+	mouse_press_input = Vector2.ZERO
+
+func _on_k_dash_cd_timer_timeout() -> void:
+	kdash_cd = false
